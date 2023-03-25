2505c5a4734114af133584b3a6bc71b438da2e98
Joe Fogiato <joe.fogiato@gmail.com>
2023-03-25 19:43:39 -0400

    Add: propTypes to the MovieTyle component

--- src/components/MovieTile/MovieTile.js
+++ src/components/MovieTile/MovieTile.js
@@ -1,12 +1,30 @@
 
-import React from 'react'
-import './MovieTile.css'
+import React from 'react';
+import './MovieTile.css';
+import PropTypes from 'prop-types';
+import noPosterImage from '../../images/no-poster.png';
 
-function MovieTile({movieData, toggleModal}) {
-    
+function MovieTile({id, title, poster_path, toggleModal}) {
   return (
-    <img key={movieData.id} className="poster" alt={`${movieData.title}-poster`} src={movieData.poster_path} onClick={() => toggleModal(movieData.id)}/>
-  )
+    <img 
+      key={id}
+      className="poster"
+      alt={`${title} poster`}
+      src={poster_path}
+      onClick={() => toggleModal(id)}
+    />
+  );
 }
 
-export default MovieTile
+export default MovieTile;
+
+MovieTile.propTypes = {
+  id: PropTypes.number,
+  title: PropTypes.string,
+  poster_path: PropTypes.string,
+  toggleModal: PropTypes.func
+};
+
+MovieTile.defaultProps = {
+  poster_path: noPosterImage
+}
